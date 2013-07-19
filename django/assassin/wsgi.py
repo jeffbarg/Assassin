7e28f9c1cf44c750de8efd36ce39a1201c98a85e
Jeff Barg <jeffbarg@wharton.upenn.edu>
2013-07-18 22:32:01 -0400

    Updated wsgi file to include django directory in working directory

--- django/assassin/wsgi.py
+++ django/assassin/wsgi.py
@@ -14,6 +14,10 @@ framework.
 
 """
 import os
+import sys
+
+sys.path.append('var/www/assassin.jeffbarg.com/django')
+sys.path.append('/var/www/assassin.jeffbarg.com/django/assassin')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
