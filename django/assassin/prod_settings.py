877c227ebc9f22904bed155b8904ba00caff39f1
Jeff Barg <jeffbarg@wharton.upenn.edu>
2013-07-18 22:52:11 -0400

    - Typo fix for production - Production update for localized paths

--- django/assassin/prod_settings.py
+++ django/assassin/prod_settings.py
@@ -52,18 +52,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/var/www/assassin.jeffbarg.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/assassin.jeffbarg.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -127,6 +127,7 @@ ROOT_URLCONF = 'assassin.urls'
 WSGI_APPLICATION = 'assassin.wsgi.application'
 
 TEMPLATE_DIRS = (
+	'/var/www/assassin.jeffbarg.com/templates/',
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
--- django/assassin/wsgi.py
+++ django/assassin/wsgi.py
@@ -16,14 +16,14 @@ framework.
 import os
 import sys
 
-sys.path.append('var/www/assassin.jeffbarg.com/django')
-sys.path.append('/var/www/assassin.jeffbarg.com/django/assassin')
+sys.path.append('/var/www/assassin.jeffbarg.com/django/')
+sys.path.append('/var/www/assassin.jeffbarg.com/django/assassin/')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "assassin.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assassin.production_settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "assassin.settings"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assassin.production_settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
