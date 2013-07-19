322a474dd3ce849eb756437358d37f2c7c41b17e
Jeff Barg <jeffbarg@wharton.upenn.edu>
2013-07-18 23:02:57 -0400

    Final fixes for wsgi file

--- django/assassin/wsgi.py
+++ django/assassin/wsgi.py
@@ -22,8 +22,8 @@ sys.path.append('/var/www/assassin.jeffbarg.com/django/assassin/')
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-os.environ["DJANGO_SETTINGS_MODULE"] = "assassin.settings"
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assassin.production_settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "assassin.prod_settings"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assassin.prod_settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
